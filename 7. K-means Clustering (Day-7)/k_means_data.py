6fd5658e7fbb63583df944ffb34919126f4f8e25
Prabhat Kumar Sahu <prabhat1015@gmail.com>
2018-07-12 18:16:27 +0530

    Add files via upload

---
+++ 7. K-means Clustering (Day-7)/k_means_data.py
@@ -0,0 +1,34 @@
+ # clustering dataset
+from sklearn.cluster import KMeans
+from sklearn import metrics
+import numpy as np
+import matplotlib.pyplot as plt
+
+x1 = np.array([3, 1, 1, 2, 1, 6, 6, 6, 5, 6, 7, 8, 9, 8, 9, 9, 8])
+x2 = np.array([5, 4, 6, 6, 5, 8, 6, 7, 6, 7, 1, 2, 1, 2, 3, 2, 3])
+
+plt.plot()
+plt.xlim([0, 10])
+plt.ylim([0, 10])
+plt.title('Dataset')
+plt.scatter(x1, x2)
+plt.show()
+
+# create new plot and data
+plt.plot()
+X = np.array(list(zip(x1, x2))).reshape(len(x1), 2)
+colors = ['b', 'g', 'r']
+markers = ['o', 'v', 's']
+
+# KMeans algorithm 
+K = 3
+kmeans_model = KMeans(n_clusters=K).fit(X)
+
+plt.plot()
+for i, l in enumerate(kmeans_model.labels_):
+    plt.plot(x1[i], x2[i], color=colors[l], marker=markers[l],ls='None')
+    plt.xlim([0, 10])
+    plt.ylim([0, 10])
+
+plt.show()
+ 
